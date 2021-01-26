87de62fad62dbce3d34255bdf8f16b0742e3d0b2
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-01-26 05:39:37 -0500

    Made flexible

--- FeatureSelectionPlugin.R
+++ FeatureSelectionPlugin.R
@@ -6,8 +6,16 @@ input <- function(inputfile) {
 parameters <<- read.table(inputfile, as.is=T);
   rownames(parameters) <<- parameters[,1];
   print("READING INPUT FILES...");
-  t1 <<- read.table(toString(parameters["training",2]), sep = "\t", header =FALSE, stringsAsFactors=FALSE)#, nrow=20000)
-  my_rma <<- read.table(toString(parameters["clinical",2]), sep="\t", header = FALSE)
+  my_rma <<- read.table(toString(parameters["training",2]), sep = "\t", header =FALSE, stringsAsFactors=FALSE)#, nrow=20000)
+  t1 <<- read.table(toString(parameters["clinical",2]), sep="\t", header = TRUE, stringsAsFactors=FALSE)
+  joinby <<- toString(parameters["joinby", 2])
+  case <<- toString(parameters["case", 2])
+  control <<- toString(parameters["control", 2])
+  targetcol <<- toString(parameters["targetcol", 2])
+  targetval <<- toString(parameters["targetval", 2])
+  timecol <<- toString(parameters["timecol", 2])
+  timestart <<- as.double(toString(parameters["timestart", 2]))
+  timeend <<- as.double(toString(parameters["timeend", 2]))
   print("DONE");
   my_rma <<- as.data.frame(t(my_rma))
 } 
@@ -20,18 +28,17 @@ control_number=20
 pv=0.9
 fch=2
 
-g1=rep("T",case_number)
-g2=rep("N",control_number)
+g1=rep(case,case_number)
+g2=rep(control,control_number)
 
 groups=as.factor(c(g1,g2))
 design<-model.matrix(~groups)
-
-v1 <- t1[t1[,"STUDYID"] == "DEE1 RSV",]
-v11 <- v1[v1[,"TIMEHOURS"]==0,]
-v12 <- v1[v1[,"TIMEHOURS"]==-24,]
+v1 <- t1[t1[,targetcol] == targetval,]
+v11 <- v1[v1[,timecol]==timeend,]
+v12 <- v1[v1[,timecol]==timestart,]
 v13 <- rbind(v11, v12)
-colnames(my_rma)[1] <- "CEL"
-fin<-merge(v13, my_rma, by = "CEL")
+colnames(my_rma)[1] <- joinby
+fin<-merge(v13, my_rma, by = joinby)
 cor <- fin[,13:ncol(fin)]
 
 cor <- sapply(cor, as.numeric)
@@ -50,27 +57,5 @@ tt<<-topTable(fit,coef=2,number=nrow(cor), adjust.method="BH",sort.by="B",p.valu
 output <- function(outputfile) {
 write.table(tt, file = outputfile, row.names=TRUE)
 }
-#chosen_genes <- row.names(tt)
-
-#Combining Age, Gender and Shedding param and doing a Random Forest
-
-#fin2 <- merge(t1, my_rma, by = "CEL")
-#genes <- fin2[,c(4, 13:ncol(fin2), 8)]
-#AGES <- t1[, c(3, 4, 5, 7)]
-#AGES[is.na(AGES)]<-0
-
-#library(randomForest)
-#library(rpart)
-#library(rattle)
-
-#smp_size <- floor(0.75 * nrow(AGES))
-#train_ind <- sample(seq_len(nrow(AGES)), size = smp_size)
-
-#train <- AGES[train_ind, ]
-#test <- AGES[-train_ind, ]
-#fit<- randomForest(as.factor(SHEDDING_SC1)~ AGE + GENDER + EARLYTX, data = train, importance = TRUE, ntree = 10 )
-#my_prediction <- predict(fit,test)
-#fit$confusion
-#fancyRpartPlot(fit)
 
 
